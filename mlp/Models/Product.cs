295c00b76d38c497840955f31c89937df560a208
anmesk <an_mesk@live.com>
2020-02-14 14:09:40 -0500

    Added Product class properties

--- mlp/Models/Product.cs
+++ mlp/Models/Product.cs
@@ -7,5 +7,12 @@ namespace mlp.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Category { get; set; }
+        public string Review { get; set; }
+        public DateTime DateTimeGroup { get; set; }
+        public string Description { get; set; }
     }
 }
