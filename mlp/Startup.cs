2d913e5fe4badc767fc675d5aa95f1513d422791
anmesk <an_mesk@live.com>
2020-02-14 15:12:54 -0500

    Added Index.cshtml view

--- mlp/Startup.cs
+++ mlp/Startup.cs
@@ -42,7 +42,7 @@ namespace mlp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Product}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
