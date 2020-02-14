ad5b4faa048beb33944b08b3a6186909daf91f00
dealer426 <burnssamuel01@gmail.com>
2020-02-14 14:48:10 -0500

    Added Product Repository

---
+++ mlp/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using mlp.Models;
+
+namespace mlp.Repositories
+{
+    public class ProductRepository : IRepository<Product>
+    {
+
+
+        public IEnumerable<Product> GetAll()
+        {
+            return productDictionary.Values;
+        }
+    }
+}
