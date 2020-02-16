5d21e242420e885adcf7f55e07b16b36c1a3e824
anmesk <an_mesk@live.com>
2020-02-16 14:37:08 -0500

    Added more ponies

--- mlp/Repositories/ProductRepository.cs
+++ mlp/Repositories/ProductRepository.cs
@@ -15,6 +15,9 @@ namespace mlp.Repositories
             productDictionary = new Dictionary<int, Product>()
             {
                 { 1, new Product(1, "Rainbow Dash", "rainbowdash.png", "Leader", "This is my review", DateTime.Now, "I am a strong Pony! Haaaaay!") },
+                { 2, new Product(2, "Fluttershy", "fluttershy.png", "Pony", "This is another review", DateTime.Now, "I am a shy Pony... hello!") },
+                { 3, new Product(3, "Pinkie Pie", "pinkiepie.png", "Pony", "This is yet another review", DateTime.Now, "I am a fun Pony!!!") },
+                { 4, new Product(4, "Twilight Sparkle", "twilightsparkle.png", "Pony", "This is yet yet another review", DateTime.Now, "I am yet another pony, heya!") },
             };
         }
 
