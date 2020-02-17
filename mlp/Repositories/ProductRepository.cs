beb535c9f58925af362fc5041c1f961fab51214b
Jason <JSilvis030592@yahoo.com>
2020-02-17 14:32:10 -0500

    Added a little sparkle

--- mlp/Repositories/ProductRepository.cs
+++ mlp/Repositories/ProductRepository.cs
@@ -14,10 +14,10 @@ namespace mlp.Repositories
         {
             productDictionary = new Dictionary<int, Product>()
             {
-                { 1, new Product(1, "Rainbow Dash", "rainbowdash.jpg", "Leader", "This is my review", DateTime.Now, "I am a strong Pony! Haaaaay!") },
-                { 2, new Product(2, "Fluttershy", "fluttershy.jpg", "Pony", "This is another review", DateTime.Now, "I am a shy Pony... hello!") },
-                { 3, new Product(3, "Pinkie Pie", "pinkiepie.jpg", "Pony", "This is yet another review", DateTime.Now, "I am a fun Pony!!!") },
-                { 4, new Product(4, "Twilight Sparkle", "twilightsparkle.jpg", "Pony", "This is yet yet another review", DateTime.Now, "I am yet another pony, heya!") },
+                { 1, new Product(1, "Rainbow Dash", "rainbowdash.png", "Leader", "This is my review", DateTime.Now, "I am a strong Pony! Haaaaay!") },
+                { 2, new Product(2, "Fluttershy", "fluttershy.png", "Pony", "This is another review", DateTime.Now, "I am a shy Pony... hello!") },
+                { 3, new Product(3, "Pinkie Pie", "pinkypie.png", "Pony", "This is yet another review", DateTime.Now, "I am a fun Pony!!!") },
+                { 4, new Product(4, "Twilight Sparkle", "twilightsparkle.png", "Pony", "This is yet yet another review", DateTime.Now, "I am yet another pony, heya!") },
             };
         }
 
